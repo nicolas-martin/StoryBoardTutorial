161797d8bcb40cd34386b76890957c42ea9e8851
Nicolas Martin <nicolas.martin.0986@gmail.com>
2015-06-28 02:33:04 -0400

    Add game

--- Ratings/GamePickerViewController.swift
+++ Ratings/GamePickerViewController.swift
@@ -1,40 +1,25 @@
 //
-//  PlayersViewController.swift
+//  GamePickerViewController.swift
 //  Ratings
 //
-//  Created by Nicolas Martin on 2015-06-27.
+//  Created by Nicolas Martin on 2015-06-28.
 //  Copyright (c) 2015 Nicolas Martin. All rights reserved.
 //
 
 import UIKit
 
-class PlayersViewController: UITableViewController {
-    var players: [Player] = playersData
+class GamePickerViewController: UITableViewController {
 
+    var games:[String]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
-    }
-    
-    @IBAction func cancelToPlayersViewController(segue:UIStoryboardSegue) {
-        
-    }
-    
-    @IBAction func savePlayerDetail(segue:UIStoryboardSegue) {
-        if let playerDetailsViewController = segue.sourceViewController as? PlayerDetailsViewController {
-            
-            //add the new player to the players array
-            players.append(playerDetailsViewController.player)
-            
-            //update the tableView
-            let indexPath = NSIndexPath(forRow: players.count-1, inSection: 0)
-            tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
-        }
+        games = ["Angry Birds",
+            "Chess",
+            "Russian Roulette",
+            "Spin the Bottle",
+            "Texas Hold'em Poker",
+            "Tic-Tac-Toe"]
     }
 
     override func didReceiveMemoryWarning() {
@@ -47,40 +32,16 @@ class PlayersViewController: UITableViewController {
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
     }
-
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return players.count
-    }
-
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath)
-        -> UITableViewCell {
-            let cell = tableView.dequeueReusableCellWithIdentifier("PlayerCell", forIndexPath: indexPath)
-                as! PlayerCell
-            
-            let player = players[indexPath.row] as Player
-            cell.nameLabel.text = player.name
-            cell.gameLabel.text = player.game
-            cell.ratingImageView.image = imageForRating(player.rating)
-            return cell
+        return games.count
     }
     
-    func imageForRating(rating:Int) -> UIImage? {
-        switch rating {
-        case 1:
-            return UIImage(named: "1StarSmall")
-        case 2:
-            return UIImage(named: "2StarsSmall")
-        case 3:
-            return UIImage(named: "3StarsSmall")
-        case 4:
-            return UIImage(named: "4StarsSmall")
-        case 5:
-            return UIImage(named: "5StarsSmall")
-        default:
-            return nil
-        }
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("GameCell", forIndexPath: indexPath) as! UITableViewCell
+        cell.textLabel?.text = games[indexPath.row]
+        return cell
     }
-
 
     /*
     // Override to support conditional editing of the table view.
---
+++ Ratings/PlayerDetailsViewController.swift
@@ -0,0 +1,56 @@
+//
+//  PlayerDetailsViewController.swift
+//  Ratings
+//
+//  Created by Nicolas Martin on 2015-06-28.
+//  Copyright (c) 2015 Nicolas Martin. All rights reserved.
+//
+
+import UIKit
+
+class PlayerDetailsViewController: UITableViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var detailLabel: UILabel!
+    var player:Player!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        println("init PlayerDetailsViewController")
+        super.init(coder: aDecoder)
+    }
+    
+    deinit {
+        println("deinit PlayerDetailsViewController")
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        if indexPath.section == 0 {
+            nameTextField.becomeFirstResponder()
+        }
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "SavePlayerDetail" {
+            player = Player(name: self.nameTextField.text, game: "Chess", rating: 1)
+        }
+    }
+
+
+
+}
